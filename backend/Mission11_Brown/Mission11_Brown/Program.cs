43ff6d1667a449d4ba9803d7ab4d585373858260
barlee1 <kyleeb2255@gmail.com>
2025-04-04 09:44:46 -0600

    newchanges

--- backend/Mission11_Brown/Mission11_Brown/Program.cs
+++ backend/Mission11_Brown/Mission11_Brown/Program.cs
@@ -18,9 +18,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000", "https://victorious-sand-0508eab1e.6.azurestaticapps.net") // Allow your React app
-                .AllowAnyHeader()
-                .AllowAnyMethod();
+            policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+
         });
 });
 
--- backend/Mission11_Brown/Mission11_Brown/obj/Release/net8.0/Mission11_Brown.AssemblyInfo.cs
+++ backend/Mission11_Brown/Mission11_Brown/obj/Release/net8.0/Mission11_Brown.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Brown")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c38b55dd2c4240eec65e3896dc3e778cda2ac4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c941ef7ad0105f21ee3c127b8f651b7271de65f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Brown")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Brown")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
