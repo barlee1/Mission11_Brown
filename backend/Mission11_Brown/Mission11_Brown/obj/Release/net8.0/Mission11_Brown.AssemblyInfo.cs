7c26feeeedad71599d2cc7d814e8131668e8058b
barlee1 <kyleeb2255@gmail.com>
2025-04-03 22:06:00 -0600

    more deployment

--- backend/Mission11_Brown/Mission11_Brown/obj/Release/net8.0/Mission11_Brown.AssemblyInfo.cs
+++ backend/Mission11_Brown/Mission11_Brown/obj/Release/net8.0/Mission11_Brown.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Brown")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2fa2afbb255987dc23c6331b15424fd9a86e3b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c38b55dd2c4240eec65e3896dc3e778cda2ac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Brown")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Brown")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
