ce9ca8d3041d8009068835590ea8723b95647b43
barlee1 <kyleeb2255@gmail.com>
2025-04-04 09:47:10 -0600

    updated url

--- frontend/src/api/BooksApi.ts
+++ frontend/src/api/BooksApi.ts
@@ -6,7 +6,7 @@ interface FetchBooksResponse {
 }
 
 const API_URL =
-  "https://mission13-brown-backend-2-bxbkatbeecfzc8dc.eastus-01.azurewebsites.net/Book";
+  "https://mission13-brown-backend-gjgcb7abhze5btcp.eastus-01.azurewebsites.net/book";
 
 // collecting every book
 export const fetchBooks = async (
